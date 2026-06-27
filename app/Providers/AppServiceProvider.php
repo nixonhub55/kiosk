934df00f8a88aef603981e07723d197ae8335523
nixonhub55 <onnix2559@gmail.com>
2026-06-27 14:51:37 +0800

    Update AppServiceProvider.php
    wow

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            // Force root to /kiosk
+            URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        } 
+    /*  
+        if (env('FORCE_HTTPS', false)) {
+        URL::forceScheme('https');
+    }
+        */
     }
 }
